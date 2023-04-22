6cc003f70a542a7eda009c5e04992a9ade70237e
TheColdPot <guohw979@outlook.com>
2023-04-22 21:22:57 +0800

    Initial commit

---
+++ src/main/java/io/github/arcticpot/jublockly/Jublockly.java
@@ -0,0 +1,13 @@
+package io.github.arcticpot.jublockly;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Jublockly implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
