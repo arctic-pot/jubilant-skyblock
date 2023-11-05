f576a0aea0d007cd6ffb2a59183fda547a5efb15
TheColdPot <guohw979@outlook.com>
2023-11-05 20:55:06 +0800

    style: change the name for easier understanding

--- src/main/kotlin/io/github/arcticpot/jublockly/base/data/StatValueCalculation.kt
+++ src/main/kotlin/io/github/arcticpot/jublockly/base/data/StatValueCalculation.kt
@@ -1,7 +1,10 @@
 package io.github.arcticpot.jublockly.base.data
 
-class StatValueCalculation<T>(vararg val dependencies: StatValue<*>, val function: () -> T): StatValueLike<T> {
-    override val value get() = function()
+class StatValueCalculation<T>(private vararg val dependencies: StatValue<*>, private val evaluator: () -> T): StatValueLike<T> {
+    override val value get() = evaluator.invoke()
 
-    override val presenting get() = !dependencies.any { !it.presenting }
+    override val presenting get() = dependencies.all { it.presenting }
+    
+//    If it causes trouble...
+//    override val presenting get() = dependencies[0].presenting
 }
